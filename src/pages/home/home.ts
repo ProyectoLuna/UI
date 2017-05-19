9097a895c928de39362326ddcf28af831b0c4461
TurBoss <j.l.toledano.l@gmail.com>
2017-05-19 03:14:09 +0200

    missing line

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
 
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
 import { Http, Headers, RequestOptions } from '@angular/http';
 import 'rxjs/add/operator/map';
+
+@IonicPage()
 
 @Component({
   selector: 'page-home',
