33dfc6979d01de96443d0919a87ec1c5aed560bf
TurBoss <j.l.toledano.l@gmail.com>
2017-05-19 08:23:22 +0200

    Add config security

--- src/pages/config/config.ts
+++ src/pages/config/config.ts
@@ -1,24 +1,39 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { NativeStorage } from '@ionic-native/native-storage';
 
-/**
- * Generated class for the ConfigPage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
+
 @IonicPage()
+
 @Component({
   selector: 'page-config',
   templateUrl: 'config.html',
 })
+
 export class ConfigPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              private nativeStorage: NativeStorage)
+  {
+
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad ConfigPage');
+  ionViewCanEnter() {
+    this.nativeStorage.getItem('logged_in')
+      .then(
+        (data) => {
+          if (data['value'] == true) {
+            return true;
+          } else {
+            return false;
+          }
+        },
+        (error) => {
+          console.error(error);
+          return false;
+        }
+      );
   }
 
 }
