d22c3375b671799b9c270e4f626db0b840cc5c66
TurBoss <j.l.toledano.l@gmail.com>
2017-05-21 20:00:36 +0200

    Toggle button

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -22,13 +22,64 @@ export class HomePage {
   login_page:any = LoginPage;
   config_page:any = ConfigPage;
 
-  gateways: any;
+    gateways: any;
+    subscriptors: any;
+
 
   constructor(public navCtrl: NavController,
               private nativeStorage: NativeStorage,
               public http: Http)
   {
-    
+    this.check_gateway();
+    this.check_subscriptors();
+  }
+
+  check_subscriptors() {
+
+    var items = [];
+
+    var link = "https://192.168.10.13:8080/check_subscriptors";
+
+    let type: string = "application/json; charset=UTF-8",
+        headers: any = new Headers({ 'Content-Type': type}),
+        options: any = new RequestOptions({ headers: headers })
+
+    this.http.get(link, options)
+    .map(res => res.json())
+    .subscribe(data => {
+  		for(let i = 0; i < data.length; i++) {
+  		  items.push({
+  		    id: data[i]["id"],
+    			name: data[i]["name"],
+          type: data[i]["type"]
+  		  });
+  		}
+      this.subscriptors = items;
+    });
+  }
+
+  check_gateway() {
+
+    var items = [];
+
+    var link = "https://192.168.10.13:8080/check_gateway";
+
+    let type: string = "application/json; charset=UTF-8",
+        headers: any = new Headers({ 'Content-Type': type}),
+        options: any = new RequestOptions({ headers: headers })
+
+    this.http.get(link, options)
+    .map(res => res.json())
+    .subscribe(data => {
+  		for(let i = 0; i < data.length; i++) {
+  		  items.push({
+  		    id: data[i]["id"],
+    			name: data[i]["name"]
+  		  });
+  		}
+      this.gateways = items;
+      console.log(this.gateways[0].name)
+    });
   }
 
   do_logout() {
