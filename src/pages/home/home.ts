59317384c543ffe3785a261d74b163561f79acec
TurBoss <j.l.toledano.l@gmail.com>
2017-05-21 11:08:57 +0200

    ssl support

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -28,31 +28,7 @@ export class HomePage {
               private nativeStorage: NativeStorage,
               public http: Http)
   {
-    this.check_gateway();
-  }
-
-  check_gateway() {
-
-    var items = [];
-
-    var link = "http://192.168.10.13:8080/check_gateway";
-
-    let type: string = "application/json; charset=UTF-8",
-        headers: any = new Headers({ 'Content-Type': type}),
-        options: any = new RequestOptions({ headers: headers })
-
-    this.http.get(link, options)
-    .map(res => res.json())
-    .subscribe(data => {
-  		for(let i = 0; i < data.length; i++) {
-  		  items.push({
-  		    id: data[i]["id"],
-    			name: data[i]["name"]
-  		  });
-  		}
-      this.gateways = items;
-      console.log(this.gateways[0].name)
-    });
+    
   }
 
   do_logout() {
